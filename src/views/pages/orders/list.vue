95939c4fa9923f817f6183a8a0f909ff97c15882
Shakh7 <00011485wiut@gmail.com>
2022-11-23 14:41:34 +0500

    order delete

--- src/views/pages/orders/list.vue
+++ src/views/pages/orders/list.vue
@@ -2,10 +2,12 @@
 import CustomTable from '../../../components/custom/table.vue'
 import OrderApi from '@/api/orders/orders_api.js'
 import {ordersMehtods} from "@/state/helpers";
+import Swal from "sweetalert2";
 
 export default {
   data() {
     return {
+      numberOfErrors: 0,
       headers: [
         {
           label: 'ORDER NUMBER',
@@ -74,7 +76,84 @@ export default {
     setToUpdateOrder(order) {
       this.setCurrentlyUpdating(order)
       this.$router.push({name: 'orders_container_update', params: {id: order.id}})
-    }
+    },
+    deleteOrderConfirmation(order) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: `You are about to delete order ${(order.order_number).toString().toUpperCase()}`,
+        text: 'Deleting your order will remove all of its information from our database.',
+        showDenyButton: true,
+        showConfirmButton: true,
+        confirmButtonText: 'Yes, Delete It',
+        denyButtonText: 'Cancel',
+        cancelButtonColor: 'transparent',
+        focusConfirm: false,
+        inputLabel: `Please type Order${(order.order_number).toString().toUpperCase()} to confirm`,
+        input: 'email',
+        inputPlaceholder: `Order${(order.order_number).toString().toUpperCase()}`,
+        inputValidator: (value) => {
+          return new Promise((resolve) => {
+            if (value === 'Order' + (order.order_number).toString().toUpperCase()) {
+              resolve(this.deleteOrder(order))
+            } else {
+              resolve('Order number did not match :)')
+            }
+          })
+        }
+      });
+    },
+
+    deleteOrder(order) {
+      fetch(`${process.env.VUE_APP_ORDER_URL}/container_order/list/${order.order_number}/delete/`, {
+        method: 'DELETE',
+      }).then(response => {
+        if (response.ok) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Order has been deleted successfully',
+            showConfirmButton: false,
+            timer: 3000
+          })
+          this.getOrders()
+        } else {
+          Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'An error occurred while deleting order',
+            showConfirmButton: false,
+          })
+        }
+      }).catch(error => {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: `ERROR \n ${error}`,
+          showConfirmButton: true,
+          confirmButtonText: 'Try Again',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            if (this.numberOfErrors >= 2) {
+              Swal.fire({
+                position: 'center',
+                icon: 'warning',
+                title: `Too Many Tries...\nPlease, talk to IT department to fix the proplem`,
+                showConfirmButton: false,
+                timer: 10000,
+                willClose: () => {
+                  window.location.reload()
+                }
+              })
+            } else {
+              this.numberOfErrors += 1
+              this.deleteOrderConfirmation(order)
+            }
+          }
+        })
+        console.log(error)
+      })
+    },
   },
   async mounted() {
     await this.getOrders()
@@ -128,7 +207,8 @@ export default {
       <font-awesome-icon @click="setToUpdateOrder(slotProps.row)" icon="fa-solid fa-pen-to-square"
                          class="c_icon mx-2 c_icon_hoverable"/>
 
-      <font-awesome-icon icon="fa-solid fa-trash" class="c_icon c_icon_hoverable text-danger"/>
+      <font-awesome-icon @click="deleteOrderConfirmation(slotProps.row)"
+                         icon="fa-solid fa-trash" class="c_icon c_icon_hoverable text-danger"/>
     </template>
 
     <template v-slot:manager="slotProps">
