3c63ca504925b222ce13dca7a3b66081584e5bf7
Shakh7 <00011485wiut@gmail.com>
2022-11-26 16:08:42 +0500

    vue app url for production

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    devServer: {
-        allowedHosts: "all"
-    }
-})
+module.exports = {
+  devServer: {
+     disableHostCheck: true
+  } }
