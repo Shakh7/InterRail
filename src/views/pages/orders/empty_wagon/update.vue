99baa0bbc8d7dca0615baa6ed9d54b448174bd0a
Shakh7 <00011485wiut@gmail.com>
2022-12-26 10:55:08 +0500

    empty wagon order update

--- src/views/pages/orders/empty_wagon/update.vue
+++ src/views/pages/orders/empty_wagon/update.vue
@@ -200,7 +200,6 @@ export default {
 
 <template>
 
-  {{ currentOrder }}
   <custom_wizard wizard_header="Update order" :steps="steps" v-if="hasData">
     <template v-slot:content-step1-body>
       <div class="row g-3">
