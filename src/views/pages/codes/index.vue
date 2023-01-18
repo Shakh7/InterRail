38b2ad56edbf3a8f77c00bc972a1946385bff595
Shakh7 <00011485wiut@gmail.com>
2023-01-18 18:42:31 +0500

    codes

---
+++ src/views/pages/codes/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import DataTable from 'datatables.net-vue3';
+import DataTablesLib from 'datatables.net-responsive-bs5';
+
+DataTable.use(DataTablesLib);
+
+// const columns = [
+//   {data: 'name'},
+//   {data: 'position'},
+// ];
+const data = [
+  [1, 2],
+  [3, 4],
+];
+</script>
+
+<template>
+  <div class="card card-body">
+    <DataTable
+        :data="data"
+        class="table table-hover table-striped display"
+        width="100%"
+    >
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Position</th>
+      </tr>
+      </thead>
+    </DataTable>
+  </div>
+</template>
