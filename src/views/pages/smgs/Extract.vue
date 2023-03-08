cbeeead9c68fa7ba678f42fdd5167eb763ef4175
Dilmurod Yangiboev <dilmurod.yangiboev@gmail.com>
2023-03-08 12:48:00 +0100

    get results from fetch

--- src/views/pages/smgs/Extract.vue
+++ src/views/pages/smgs/Extract.vue
@@ -495,10 +495,10 @@ export default {
     },
     async getCodeOptions(query) {
       if (query.length < 2) {return}
-      let res = await fetch(`${process.env.VUE_APP_ORDER_URL}/code/extractor/list/?forwarder_id=${this.forwarder.selected.id}&number=${query}`).results
+      let res = await fetch(`${process.env.VUE_APP_ORDER_URL}/code/extractor/list/?forwarder_id=${this.forwarder.selected.id}&number=${query}`)
       let result = await res.json()
       this.code.selected = null
-      this.code.options = result
+      this.code.options = result.results
     },
     async confirm() {
 
